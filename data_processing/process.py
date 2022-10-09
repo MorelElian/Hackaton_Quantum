b9d1f10437dbd8dc91d0314713238c9b8de9d9c7
MorelElian <elian.morel@laposte.net>
2022-10-09 09:27:53 +0000

    add segmentation processing

--- data_processing/process.py
+++ data_processing/process.py
@@ -1,8 +1,10 @@
 import pandas as pd 
+import numpy as np
 import os
 import shutil
 import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator 
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img
 def create_label_folder(
   path : str,
   path_csv : str,
@@ -80,4 +82,54 @@ def generate_test(
       tensor = tf.expand_dims(tensor, axis=0)
       array_unclassified.append(tensor)
       return array_unclassified
+
+class Silos(keras.utils.Sequence):
+    """Helper to iterate over the data (as Numpy arrays)."""
+
+    def __init__(self, batch_size, img_size, input_img_paths, input_mask_paths):
+        self.batch_size = batch_size
+        self.img_size = img_size
+        self.input_img_paths = input_img_paths
+        self.input_mask_paths = input_mask_paths
+
+    def __len__(self):
+        return len(self.input_mask_paths) // self.batch_size
+
+    def __getitem__(self, idx):
+        """Returns tuple (input, target) correspond to batch #idx."""
+        i = idx * self.batch_size
+        batch_input_img_paths = self.input_img_paths[i : i + self.batch_size]
+        batch_target_img_paths = self.input_mask_paths[i : i + self.batch_size]
+        x = np.zeros((self.batch_size,) + self.img_size , dtype="float32")
+        for j, path in enumerate(batch_input_img_paths):
+            img = load_img(path, target_size=self.img_size)
+            x[j] = img
+        y = np.zeros((self.batch_size,) + self.img_size , dtype="float32")
+        for j, path in enumerate(batch_target_img_paths):
+            img = load_img(path, target_size=self.img_size, color_mode="grayscale")
+            y[j] = np.expand_dims(img, 2)
+            # Ground truth labels are 1, 2, 3. Subtract one to make them 0, 1, 2:
+            y[j] /= 255
+        return x, y
+
+def process_data_for_segmentation(path):
+
+  """Generate dataset of Silos objects in order to train and test the segmentation model"""
+  image_path = os.path.join(path, 'images/')
+  mask_path = os.path.join(path, 'masks/')
+  xai_data = pd.read_csv(os.path.join(path,'x-ai_data.csv'))
+  train = [row[0] for (i,row) in xai_data.iterrows() if (row[2]=='train' and row[1]==1)]
+  valid = [row[0] for (i,row) in xai_data.iterrows() if (row[2]=='validation' and row[1]==1)]
+  test = [row[0] for (i,row) in xai_data.iterrows() if (row[2]=='test')]
+
+  image_list_train = [image_path+i for i in train]
+  mask_list_train = [mask_path+i for i in train]
+  image_list_test = [image_path+i for i in test]
+  mask_list_test = [mask_path+i for i in test]
+  image_list_valid = [image_path+i for i in valid]
+  mask_list_valid = [mask_path+i for i in valid]
+
+  datasets_train = Silos(batch_size=32,img_size=(256,256,3),input_img_paths = image_list_train,input_mask_paths = mask_list_train)
+  datasets_valid = Silos(batch_size=32,img_size=(256,256,3),input_img_paths = image_list_valid,input_mask_paths = mask_list_valid)
+  return datasets_train, datasets_valid
     
