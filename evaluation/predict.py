40c164225012ba129a05ba197538a00dd3a444ad
navaxel <axel.navarro@polytechnique.edu>
2022-10-09 05:20:04 +0000

    predict + auc

--- evaluation/predict.py
+++ evaluation/predict.py
@@ -1,17 +1,24 @@
 import numpy as np
 
-def evaluate(model_fit,
+def proba(model_fit,
             tensors_silos,
-            tensor_pas_silos,
-            threshold=0.5):
+            tensor_pas_silos):
+  probas_silos = [model_fit.predict_step(t) for t in tensors_silos]
+  probas_pas_silos = [model_fit.predict_step(t) for t in tensors_pas_silos]
 
-  y_pred_on_silos = [1 if model_fit.predict_step(t) > threshold else 0 for t in tensor_silos]
+  return probas_silos, probas_pas_silos
+
+def evaluate(probas_silos,
+            probas_pas_silos,
+            threshold=0.5):
+    
+  y_pred_on_silos = [1 if p > threshold else 0 for p in probas_silos]
   
   NP = len(y_pred_on_silos)
   TP = sum(y_pred_on_silos)
   FN = NP - TP
 
-  y_pred_on_pas_silos = [1 if model_fit.predict_step(t) > threshold else 0 for t in tensor_pas_silos]
+  y_pred_on_pas_silos = [1 if p > threshold else 0 for p in probas_pas_silos]
 
   NN = len(y_pred_on_pas_silos)
   FP = sum(y_pred_on_pas_silos)
@@ -38,7 +45,8 @@ def evaluate(model_fit,
 
 def predict(model_fit,
 tensor,
-threshold=0.5):
+threshold=0.5,
+proba=False):
   y_pred = []
   for t in tensor:
     y_pred.append(1 if model_fit.predict_step(t) > threshold else 0)
--- vizualisation/visu.py
+++ vizualisation/visu.py
@@ -1,24 +1,40 @@
 import pandas as pd
 import ploty.express as px
-from Hackaton_Quantum.evaluation.predict import evaluate
+import numpy as np
+from Hackaton_Quantum.evaluation.predict import proba, evaluate
+
+from sklearn.metrics import roc_auc_score
 
 def prompt_roc_curve(vgg_model,
                      inception_model,
                      silos,
                      pas_silos,
                      pas = 15):
+  
   df_results = pd.DataFrame(columns = ['confusion_matrix','precision','recall','F1-score','TPR','FNR','TNR','FPR','k','model'])
+  
+  vgg_probas_silos, vgg_probas_pas_silos = proba(vgg_model, silos, pas_silos)
+  
+  inc_probas_silos, inc_probas_pas_silos = proba(inception_model, silos, pas_silos)
+
+  vgg_auc = roc_auc_score(np.array([[p1, 1-p1] for p1 in vgg_probas_silos] + [[p0, 1-p0] for p0 in vgg_probas_pas_silos]))
+  inc_auc = roc_auc_score(np.array([[p1, 1-p1] for p1 in inc_probas_silos] + [[p0, 1-p0] for p0 in inc_probas_pas_silos]))
+  
   for k in range(1,pas):
-    _threshold = (float(k) ) / pas
-    dict_result = evaluate(vgg_model,silos,pas_silos,threshold = _threshold)
+    _threshold = (float(k)) / pas
+    dict_result = evaluate(vgg_probas_silos, vgg_probas_pas_silos, threshold =_threshold)
     dict_result['k'] = k
     dict_result['model'] ='vgg_model'
+    dict_result['auc'] = vgg_auc
     df_results.loc[2*(k-1)] = list(dict_result.values())
-    dict_result = evaluate(inception_model,silos,pas_silos,threshold = _threshold)
+    
+    dict_result = evaluate(inc_probas_silos, inc_probas_pas_silos, threshold = _threshold)
     dict_result['k'] = k
     dict_result['model'] ='inception_model'
+    dict_result['auc'] = inc_auc
     df_results.loc[2*(k-1)+1] = list(dict_result.values())
-    px.line(df_results, x = 'FPR', y = 'TPR',color = 'model').show()
-    px.line(df_results, x = 'recall', y = 'precision',color = 'model').show()
-    px.line(df_results, x = 'k',y = 'F1-score',color= 'model').show()
+
+    #px.line(df_results, x = 'FPR', y = 'TPR',color = 'model').show()
+    #px.line(df_results, x = 'recall', y = 'precision',color = 'model').show()
+    #px.line(df_results, x = 'k',y = 'F1-score',color= 'model').show()
   return df_results
